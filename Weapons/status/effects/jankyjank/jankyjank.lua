d94bcc035ddb72fc123e40d67aeaa112b1bccd5c
ok yay <72277679+ok-yay@users.noreply.github.com>
2023-09-21 23:44:16 -0500

    lol

--- Weapons/status/effects/jankyjank/jankyjank.lua
+++ Weapons/status/effects/jankyjank/jankyjank.lua
@@ -2,8 +2,9 @@ require "/scripts/util.lua" -- i suck at doing starb
 require "/scripts/status.lua"
 
 function init()
-    if (status.stat("electricResistance"<0.40)) then
+    if (status.stat("electricResistance")<0.40) then
         status.addEphemeralEffect("electricvulnerability")
+        status.addEphemeralEffect("electrified")
     end
 end
 
